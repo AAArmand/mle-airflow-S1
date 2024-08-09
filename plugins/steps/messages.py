64f5d4f84797ce5df7d0d10630640d95377de926
AAArmand <armand.airapetyan@yandex.ru>
2024-08-09 06:49:45 +0000

    Add clean_churn

--- plugins/steps/messages.py
+++ plugins/steps/messages.py
@@ -1,15 +1,16 @@
+import os
 from airflow.providers.telegram.hooks.telegram import TelegramHook # импортируем хук телеграма
 
 def send_telegram_success_message(context): # на вход принимаем словарь со контекстными переменными
     hook = TelegramHook(telegram_conn_id='test',
-                        token='*',
-                        chat_id='*')
+                        token=os.environ.get('TG_TOKEN'),
+                        chat_id=os.environ.get('TG_CHAT_ID'))
     dag = context['dag']
     run_id = context['run_id']
     
     message = f'Исполнение DAG {dag} с id={run_id} прошло успешно!' # определение текста сообщения
     hook.send_message({
-        'chat_id': '*',
+        'chat_id': os.environ.get('TG_CHAT_ID'),
         'text': message
     })
 
@@ -17,13 +18,13 @@ def send_telegram_success_message(context): # на вход принимаем �
 def send_telegram_failure_message(context):
 	# ваш код здесь #
     hook = TelegramHook(telegram_conn_id='test',
-                        token='*',
-                        chat_id='*')
+                        token=os.environ.get('TG_TOKEN'),
+                        chat_id=os.environ.get('TG_CHAT_ID'))
     key = context['task_instance_key_str']
     run_id = context['run_id']
     
     message = f'Исполнение DAG c ключом {key} с id={run_id} не прошла :(' # определение текста сообщения
     hook.send_message({
-        'chat_id': '*',
+        'chat_id': os.environ.get('TG_CHAT_ID'),
         'text': message
     })
